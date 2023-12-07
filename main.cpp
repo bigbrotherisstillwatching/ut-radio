ba2027f90c0a9ef16f30d2eb0a0f9716eac9caa9
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2023-12-07 23:22:53 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ int main(int argc, char* argv[])
     QGuiApplication* app = new QGuiApplication(argc, (char**) argv);
     app->setApplicationName("radio.s710");
 
+    qmlRegisterType<FileIO,1>("FileIO",1,0,"FileIO");
+
     QQuickView* view = new QQuickView();
     view->setSource(QUrl("qrc:/Main.qml"));
     view->setResizeMode(QQuickView::SizeRootObjectToView);
