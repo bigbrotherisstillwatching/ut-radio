2fa4dee487dc884b751a7f39719372704931dd23
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2023-12-16 16:03:52 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <QUrl>
 #include <QtQml>
 #include "fileio.h"
+#include "mytype.h"
 
 int main(int argc, char* argv[])
 {
@@ -12,6 +13,8 @@ int main(int argc, char* argv[])
     app->setApplicationName("radio.s710");
 
     qmlRegisterType<FileIO,1>("FileIO",1,0,"FileIO");
+
+    qmlRegisterType<Launcher,1>("Launcher",1,0,"Launcher");
 
     QQuickView* view = new QQuickView();
     view->setSource(QUrl("qrc:/Main.qml"));
