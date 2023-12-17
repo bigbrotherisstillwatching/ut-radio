a6d5418aba14e1f79f1be56e3840e58c877e6c18
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2023-12-17 15:10:47 +0000

    Update sigset_t.h

--- headers/sigset_t.h
+++ headers/sigset_t.h
@@ -1,7 +1,7 @@
 #ifndef __sigset_t_defined
 #define __sigset_t_defined 1
 
-#include <bits/types/__sigset_t.h>
+#include "__sigset_t.h"
 
 /* A set of signals to be blocked, unblocked, or waited for.  */
 typedef __sigset_t sigset_t;
