be000b418c8eab1250a7b4845ba0019cdd844df0
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2023-12-17 13:11:01 +0000

    Update stubs.h

--- headers/stubs.h
+++ headers/stubs.h
@@ -2,7 +2,7 @@
    This file selects the right generated file of `__stub_FUNCTION' macros
    based on the architecture being compiled for.  */
 
-#include <bits/wordsize.h>
+#include "wordsize.h"
 
 #if !defined __AARCH64EB__
 # include "stubs-lp64.h"
