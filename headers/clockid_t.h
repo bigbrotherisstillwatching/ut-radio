0f081a8eebbdca7a9a554f77a2f1c4a9b4f27496
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2023-12-17 14:08:40 +0000

    Update clockid_t.h

--- headers/clockid_t.h
+++ headers/clockid_t.h
@@ -1,7 +1,7 @@
 #ifndef __clockid_t_defined
 #define __clockid_t_defined 1
 
-#include <bits/types.h>
+#include "types.h"
 
 /* Clock ID used in clock and timer functions.  */
 typedef __clockid_t clockid_t;
