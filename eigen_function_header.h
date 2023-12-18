03e54e5379cc2f91f05dd774dae17d4d7887788d
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2023-12-18 13:02:31 +0000

    Update eigen_function_header.h

--- eigen_function_header.h
+++ eigen_function_header.h
@@ -2,13 +2,17 @@
 #define EIGEN_FUNCTION_HEADER_H
 
 #include <QObject>
-#include "headers/stdlib.h"
+#include <stdlib.h>
 
 class MyObject : public QObject{
    Q_OBJECT
 public:
     explicit MyObject (QObject* parent = 0) : QObject(parent) {}
-    Q_INVOKABLE int mymetadata() { system("cd /home/phablet/.config/radio.s710"); system("sh metadata.sh"); return 0; }
+    Q_INVOKABLE int mymetadata(){
+    system("cd /home/phablet/.config/radio.s710");
+    system("sh metadata.sh");
+    return 0;
+    }
 };
 
 #endif // EIGEN_FUNCTION_HEADER_H
