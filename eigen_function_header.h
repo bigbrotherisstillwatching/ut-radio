d3bcfcd84b49cfc2ae2b9443e3b80319ee8294db
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2023-12-18 15:00:41 +0000

    Update eigen_function_header.h

--- eigen_function_header.h
+++ eigen_function_header.h
@@ -7,7 +7,7 @@ class MyObject : public QObject{
    Q_OBJECT
 public:
     explicit MyObject (QObject* parent = 0) : QObject(parent) {}
-    Q_INVOKABLE int mymetadata()
+    Q_INVOKABLE int mymetadata();
 };
 
 #endif // EIGEN_FUNCTION_HEADER_H
