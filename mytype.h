be5f6bf036cff1cb55664f74109029ca838b16ac
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2023-12-25 01:49:30 +0100

    Create mytype.h

---
+++ mytype.h
@@ -0,0 +1,15 @@
+#ifndef LAUNCHER_H
+#define LAUNCHER_H
+#include <QObject>
+#include <QProcess>
+class Launcher : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Launcher(QObject *parent = 0);
+    ~Launcher();
+    Q_INVOKABLE QString launch(const QString &program);
+protected:
+    QProcess *m_process;
+};
+#endif // LAUNCHER_H
