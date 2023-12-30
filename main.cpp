67c98da3992de84c1b225fda932d8c4e1e21f082
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2023-12-30 23:53:00 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@
 #include <QString>
 #include <QUrl>
 #include <QtQml>
-#include "mytype.h"
+#include "process.h"
 
 int main(int argc, char* argv[])
 {
     QGuiApplication* app = new QGuiApplication(argc, (char**) argv);
     app->setApplicationName("radio.s710");
 
-    qmlRegisterType<Launcher>("Launcher", 1, 0, "Launcher");
+    qmlRegisterType<Process>( "Process", 1, 0, "Process" );
 
     QQuickView* view = new QQuickView();
     view->setSource(QUrl("qrc:/Main.qml"));
