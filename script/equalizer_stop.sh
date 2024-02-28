537b1d66aac457aaf24b8b9426fdd865e7308998
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2024-02-28 01:21:42 +0100

    Update equalizer_stop.sh

--- script/equalizer_stop.sh
+++ script/equalizer_stop.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-#pactl set-default-sink $(head -1 /home/phablet/.config/radio.s710/radio.s710.defaultsink.txt)
+pactl set-default-sink $(head -1 /home/phablet/.config/radio.s710/radio.s710.defaultsink.txt)
 
-pactl set-default-sink $(pacmd list-sinks | grep -A1 '* index:' | sed 's/.*<//; s/>.*//' | grep -v '* index:')
+#pactl set-default-sink $(pacmd list-sinks | grep -A1 '* index:' | sed 's/.*<//; s/>.*//' | grep -v '* index:')
 
 #oldsink=$(head -1 /home/phablet/.config/radio.s710/radio.s710.oldsink.txt)
 pactl unload-module $(head -1 /home/phablet/.config/radio.s710/radio.s710.oldsink.txt)
